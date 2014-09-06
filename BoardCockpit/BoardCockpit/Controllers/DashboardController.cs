83392b7cd744e2015438adf98c5261282faa201c
Jesko Iwanovski <jesko.iwanovski@nordakademie.de>
2014-09-07 00:14:07 +0200

    Dashboard AjaxLoadedData

--- BoardCockpit/BoardCockpit/Controllers/DashboardController.cs
+++ BoardCockpit/BoardCockpit/Controllers/DashboardController.cs
@@ -7,66 +7,126 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using BoardCockpit.ViewModels;
+using System.Net;
+using BoardCockpit.DAL;
+using BoardCockpit.Models;
 
 namespace BoardCockpit.Controllers
 {
     public class DashboardController : Controller
     {
         private BoardCockpitContext db = new BoardCockpitContext();
+        AjaxLoadedChart chart;
+        //List<ContextContainer> contextContailers;
+        List<Company> companies;
+        private int fromYear;
+        private int toYear;
 
         // GET: Dashboard
         public ActionResult Dashboard()
         {
+            var viewModel = new DashboardData();
+            viewModel.Companies = db.Companies.Include(i => i.ContextContainers).ToList();
+            viewModel.ContextContainers = db.ContextContainers.Include(i => i.CalculatedKPIs).Include(i => i.Contexts).Include(i => i.Company).ToList();
+
             Highcharts chart = new Highcharts("chart");
-            ViewBag.Chart = chart; 
-            return View();
+            ViewBag.Chart = AjaxLoadedChart();
+            return View(viewModel);
         }
 
-        public ActionResult AjaxLoadedChart()
+        //public ActionResult AjaxLoadedChart()
+        public Highcharts AjaxLoadedChart()
         {
-            AjaxLoadedChart chart = new Helpers.AjaxLoadedChart();
+            List<Formula> formulas = db.Formulas.ToList();
+            List<SelectListItem> items = new List<SelectListItem>();
+            //int i = 0;
+            foreach (Formula item in formulas)
+            {
+                items.Add(new SelectListItem
+                {
+                    Text = item.Name,
+                    Value = item.FormulaID.ToString()
+                });
+
+            }
+            ViewBag.Formulas = items;
+
+            var viewModel = new DashboardData();
+            viewModel.Companies = db.Companies.Include(i => i.ContextContainers).ToList();
+            viewModel.ContextContainers = db.ContextContainers.Include(i => i.CalculatedKPIs).Include(i => i.Contexts).Include(i => i.Company).ToList();
+
+            companies = db.Companies.Include(n => n.ContextContainers).ToList();
+            var data = from company in companies
+                       select company.CompanyID;
+            ViewBag.Companies = data.ToArray();
+
+            chart = new Helpers.AjaxLoadedChart();
             chart.Title = "Mein erster Titel";
             chart.Subtitle = "Mein erster Untertitel";
             List<string> years = new List<string>();
-            years.Add("2008");
-            years.Add("2009");
+            //years.Add("2008");
+            //years.Add("2009");
             chart.Categories = years;
             List<DotNet.Highcharts.Options.Series> test = new List<DotNet.Highcharts.Options.Series>();
-            test.Add(new DotNet.Highcharts.Options.Series { Name = "Test" });
+            //test.Add(new DotNet.Highcharts.Options.Series { Name = "Test" });
             chart.DataSeries = test;
             ViewBag.Graph = chart.GetChart();
-            return View();
+            return chart.GetChart();
+        }
+
+        public EmptyResult SetPeriod(string _fromYear, string _toYear)
+        {
+            fromYear = Convert.ToInt32(_fromYear);
+            toYear = Convert.ToInt32(_toYear);
+            //contextContailers = db.ContextContainers
+            //                        .Where(n => n.Year >= fromYear)
+            //                        .Where(n => n.Year <= toYear).ToList();
+
+            return new EmptyResult();
         }
 
         public JsonResult GetPeriod(string fromYear, string toYear)//(int? fromYear, int? toYear)
         {
-            List<ReportingYear> reportingYears = new List<ReportingYear>();//db.ReportingYears.ToList();
-            reportingYears.Add( new ReportingYear { ReportingYearID = 1, Year = 2008 } );
-            reportingYears.Add( new ReportingYear { ReportingYearID = 1, Year = 2009 } );
-            var data = from year in reportingYears
-                       select year.ReportingYearID;
+            List<ContextContainer> contextContainers = db.ContextContainers.ToList();
+            contextContainers.OrderBy(n => n.Year);
+            
+            //reportingYears.Add( new ReportingYear { ReportingYearID = 1, Year = 2008 } );
+            //reportingYears.Add( new ReportingYear { ReportingYearID = 1, Year = 2009 } );
+            var data = from container in contextContainers
+                       select container.Year;
+            data.Distinct();
+            
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetCompanies()//(int? fromYear, int? toYear)
+        {
+            var data = from company in companies
+                       select company.CompanyID;
 
             return Json(data, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetAjaxLoadedChartData()//(int formulaID, int companyID)
+        public JsonResult GetAjaxLoadedChartData(string formulaID, string companyID, string fromYear, string toYear)//(int formulaID, int companyID)
         {
             List<AjaxLoadedChartData> chartDatas = new List<AjaxLoadedChartData>();
+            Company company = db.Companies.Find(Convert.ToInt32(companyID));
+            List<ContextContainer> contextContainers = company.ContextContainers
+                                                                .Where(n => n.Year >= Convert.ToInt32(fromYear))
+                                                                .Where(n => n.Year <= Convert.ToInt32(toYear)).ToList();
             
-            //List<CalculatedKPI> calcKPIs = db.CalculatedKPIs
-            //                                    .Where(i => i.FormulaDetail.FormulaID == formulaID)
-            //                                    .Where(i => i.ContextContainer.CompanyID == companyID).ToList();
-            //foreach (CalculatedKPI item in calcKPIs)
-            //{
-            //    chartDatas.Add(new AjaxLoadedChartData { Year = item.ContextContainer.Year, Value = item.Value });    
-            //}
-            
-            
-
-            chartDatas.Add(new AjaxLoadedChartData { Year = 2008, Value = 10 });
-            chartDatas.Add(new AjaxLoadedChartData { Year = 2009, Value = 15000 });
-
-
+            foreach (ContextContainer item in contextContainers)
+	        {
+                CalculatedKPI calcKPI;
+                if (item.CalculatedKPIs.Where(n => n.FormulaDetail.FormulaID == Convert.ToInt32(formulaID)).Count() > 0) 
+                {
+                    calcKPI = item.CalculatedKPIs.Where(n => n.FormulaDetail.FormulaID == Convert.ToInt32(formulaID)).First();
+                    chartDatas.Add(new AjaxLoadedChartData { Year = item.Year, Value = calcKPI.Value });
+                }                 		 
+	        }
 
             return Json(chartDatas.OrderBy(i => i.Year), JsonRequestBehavior.AllowGet);
         }
---
+++ BoardCockpit/BoardCockpit/ViewModels/DashboardData.cs
@@ -0,0 +1,16 @@
+﻿using BoardCockpit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoardCockpit.ViewModels
+{
+    public class DashboardData
+    {
+        public IEnumerable<Company> Companies { get; set; }
+        public IEnumerable<ContextContainer> ContextContainers { get; set; }
+
+        //public IEnumerable<Formula> Formulas { get; set; }
+    }
+}
